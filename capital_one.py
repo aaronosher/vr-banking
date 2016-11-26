aa3626877c7b6c876d20f367c5de38fe02d86fbf
Brandon <brandonskerritt51@gmail.com>
2016-11-26 16:33:27 +0000

    updated constant api key

--- capital_one.py
+++ capital_one.py
@@ -1,11 +1,20 @@
 import requests
 
+API_KEY = '0d43177fd2d1f2a235f4222ed1cc32bd'
+
 class capitalOne:
 	@staticmethod
 	def get_user():
-		url = '''http://api.reimaginebanking.com/customers/583998b40fa692b34a9b8766/accounts?key=64af502fd1accf4c465e230fc76e0327'''
+		url = '''http://api.reimaginebanking.com/customers/583998b40fa692b34a9b8766/accounts?key={}'''.format(API_KEY)
 		data = '{"type": "Credit Card","nickname": "Way Too High APR","rewards": 700,"balance": 0}'
 		response = requests.get(url, data=data)
 		return response
+	def get_account(id):
+		url = '''api.reimaginebanking.com/accounts/{}/?key={}'''.format(id, API_KEY)
+		response = requests.get(url)
+		return response
+
 
 print(capitalOne.get_user().text)
+id = ''5839a4890fa692b34a9b8770''
+print(capitalOne.get_account(id))
