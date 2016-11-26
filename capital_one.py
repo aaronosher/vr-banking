6ddcf5064c500e931d3dd4e6ae3843689b0cd8b1
Brandon <brandonskerritt51@gmail.com>
2016-11-26 20:04:37 +0000

    Fixed Git Merge Issue
    USE BRANCHES FROM NOW ON

--- capital_one.py
+++ capital_one.py
@@ -18,8 +18,7 @@ class capitalOne:
 	# Checks if user_id is valid and then saves it to the variabls
 	def __init__(self, user_id):
 		if user_id is None:
-			print 'None'
-
+			return None
 		if self.get_customer(user_id).status_code == 200:
 			self.user_id = user_id
 
@@ -43,7 +42,7 @@ class capitalOne:
 
 		# Make request
 		url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(user_id, API_KEY)
-		response = requests.get(url)
+		response = requests.get(url).text
 
 		return response
 
@@ -52,8 +51,9 @@ class capitalOne:
 		# gets the users accounts information
 		url = '''http://api.reimaginebanking.com/accounts/{}/?key={}'''.format(user_account_id, API_KEY)
 		response = requests.get(url)
-		return response
+		return response.text
 
+	@staticmethod
 	def parse_accounts_of_users(response, user_request):
 		parsed_json = json.loads(response)
 		# gets a specific accoutn from the user using user_request
@@ -63,21 +63,23 @@ class capitalOne:
 				if user_request == value:
 					return i['_id']
 
-<<<<<<< HEAD
+	@staticmethod
 	def useful_information_account(id):
 		output = requests.get('http://api.reimaginebanking.com/accounts/{}?key={}'.format(id, API_KEY))
 		parsed_json = json.loads(output.text)
 		print(parsed_json)
 		name = parsed_json['nickname']
 		balance = parsed_json['balance']
-		if parsed_json[type] == 'Credit Card':
+		if parsed_json['type'] == 'Credit Card':
 			rewards = parsed_json['rewards']
 			useful_dict = {"Name" : name, "Balance" : balance, "Rewards" : rewards}
 		else:
 			useful_dict = {"Name": name, "Balance": balance,}
 
-capitalOne.useful_information_account('5839a4890fa692b34a9b8770')
-=======
 
 user = capitalOne('583998b40fa692b34a9b8766')
->>>>>>> 5efc1dd32d0226152ee5601f77f0930338c2965a
+response1 = user.get_accounts()
+print(capitalOne.parse_accounts_of_users(response1, "Credit Card"))
+
+capitalOne.useful_information_account(id='5839a4890fa692b34a9b8770')
+
