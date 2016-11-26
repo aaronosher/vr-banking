7d25e3a25ad20681d2500fb82d3eb76fd1d1695c
Brandon <brandonskerritt51@gmail.com>
2016-11-26 18:39:47 +0000

    Able to search for user input

--- capital_one.py
+++ capital_one.py
@@ -1,6 +1,12 @@
 import requests
 import json
 
+import logging
+
+# imports the loggign module, creates a logging file called "ProgramLog.txt"
+logging.basicConfig(filename='_ProgramLog.txt', level=logging.DEBUG,
+                    format=' %(asctime)s - %(levelname)s- %(message)s')
+
 API_KEY = '64af502fd1accf4c465e230fc76e0327'
 
 class capitalOne:
@@ -18,18 +24,25 @@ class capitalOne:
 		response = requests.get(url)
 		return response
 
-	def parse_accounts_of_users(response, user_request):
+	def parse_accounts_of_users(parsed_json, user_request):
+		logging.debug(user_request)
 		# gets a specific accoutn from the user using user_request
-		dump_json = json.dumps(response)
-		parsed_json = json.loads(response)
-		for i in parsed_json:
-			if user_request in i:
-				# if users requests is foudn in dictionary
-				account_id = i['_id']
-				# i is an interator going over every dictionary within json object
-				return account_id
 
+
+		logging.debug("parsed json is")
+		for i in parsed_json:
+			for key, value in i.items():
+				if user_request == value:
+					return i['_id']
+
+
+v = {"cat" : "the defintion of a cat"}
 
 user_account_id = '5839a4890fa692b34a9b8770'
 response1 = (capitalOne.get_user().text)
-print(capitalOne.parse_accounts_of_users(response1, user_request='rewards'))
+#dump_json = json.dumps(response1)
+
+parsed_json = json.loads(response1)
+print(parsed_json)
+
+print(capitalOne.parse_accounts_of_users(parsed_json, user_request='Credit Card'))
