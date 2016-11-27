4c4f8e33212a801308484ef47e9c3465d7b9fb3c
Brandon <brandonskerritt51@gmail.com>
2016-11-27 00:44:50 +0000

    completed owing people money alexa people pls put it in

--- alexa-bank.py
+++ alexa-bank.py
@@ -3,12 +3,11 @@ import pickle
 import os.path
 import json
 
-
 #TODO test this
 
 def owe_money(how_much):
 	# assuming how_much is a dictionary
-	if os.path.exists("owe.json", 'r'):
+	if os.path.exists("owe.json"):
 		with open("owe.json", 'r+') as fp:
 			data = json.load(fp)
 			data.append(how_much)
@@ -18,5 +17,10 @@ def owe_money(how_much):
 		with open("owe.json", 'w') as fp:
 			json.dump(how_much, fp)
 
+def how_much_do_i_owe():
+	with open("owe.json", 'r') as fp:
+		return json.load('owe.json', fp)
 
+owe = {"bethany" : "30"}
+owe_money(how_much=owe)
 
